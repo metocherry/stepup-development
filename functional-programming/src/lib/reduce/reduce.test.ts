535e9a7dbedf4dd088ee2a980d65760baf6a0cff
jinseok <metocherry@gmail.com>
2020-06-18 10:53:50 +0900

    Implement reduce function

--- functional-programming/src/lib/reduce/reduce.test.ts
+++ functional-programming/src/lib/reduce/reduce.test.ts
@@ -2,3 +2,47 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
 import reduce from "./reduce.ts";
 
+Deno.test("초기 값과 리턴 타입이 없다면, 컬렉션의 첫번째를 초기값으로 하여 순회하며, 리턴타입은 컬렉션 타입이다", () => {
+  // arrange
+  const data = [1, 2, 3];
+
+  // act
+  const actual = reduce((res, v) => res + v, data);
+
+  // assert
+  assertEquals(actual, 6);
+});
+
+Deno.test("초기 값이 없다면, 컬렉션의 첫번째를 초기값으로 하여 순회하며, 계산한다", () => {
+  // arrange
+  const data = [1, 2, 3];
+
+  // act
+  const actual = reduce<number, string>((res, v) => `${res}${v}`, data);
+
+  // assert
+  assertEquals(actual, "123");
+});
+
+Deno.test("리턴 타입이 없다면, 초기값을 시작값으로 하여 컬렉션을 순회하며, 계산하고, 리턴타입은 컬렉션 타입이다", () => {
+  // arrange
+  const data = [1, 2, 3];
+
+  // act
+  const actual = reduce((res, v) => res + v, data, 1);
+
+  // assert
+  assertEquals(actual, 7);
+});
+
+Deno.test("초기값과 리턴타입이 있다면, 초기값을 시작값으로 하여 컬렉션을 순회하며, 계산하고, 리턴타입의 객체를 리턴한다", () => {
+  // arrange
+  const data = [1, 2, 3];
+
+  // act
+  const actual = reduce<number, string>((res, v) => `${res}${v}`, data, "S");
+
+  // assert
+  assertEquals(actual, "S123");
+});
+
--- functional-programming/src/lib/reduce/reduce.ts
+++ functional-programming/src/lib/reduce/reduce.ts
@@ -1,13 +1,51 @@
-function reduce<T, R>(predicate: (result: R, data: T) => R, collection: Array<T>|Set<T>|Generator<T>, initialValue?: T|R) {
+/**
+ * 초기 값과 리턴 타입이 없다면, 컬렉션의 첫번째를 초기값으로 하여 순회하며, 리턴타입은 컬렉션 타입이다.
+ * @template T 컬렉션 타입
+ * @param predicate
+ * @param collection
+ */
+function reduce<T>(predicate: (accumulator: T, data: T) => T, collection: IterableIterator<T>|Array<T>): T;
+
+/**
+ * 초기 값이 있고, 리턴타입이 있다면, 컬렉션의 첫번째를 초기값으로 하여 순회하며 계산하고, 리턴타입의 객체를 리턴한다.
+ * @template T 컬렉션 타입
+ * @template R 리턴 타입
+ * @param predicate
+ * @param collection
+ */
+function reduce<T, R>(predicate: (accumulator: T|R, data: T) => R, collection: IterableIterator<T>|Array<T>): R;
+
+/**
+ * 리턴 타입이 없다면, 초기값을 시작 값으로 하여, 컬렉션을 순회하며 계산하고, 컬렉션 타입의 객체를 리턴한다.
+ * @template T 컬렉션 타입
+ * @param predicate
+ * @param collection
+ * @param initialValue
+ */
+function reduce<T>(predicate: (accumulator: T, data: T) => T, collection: IterableIterator<T> | Array<T>, initialValue: T): T;
+
+/**
+ * 초기값과 리턴타입이 있다면, 초기값을 시작 값으로 하여, [collection]을 순회하며 계산하고,  리턴타입의 객체를 리턴한다.
+ * @template T 컬렉션 타입
+ * @template R 리턴 타입
+ * @param predicate
+ * @param collection
+ * @param initialValue
+ */
+function reduce<T, R>(predicate: (accumulator: R, data: T) => R, collection: IterableIterator<T> | Array<T>, initialValue: R): R;
+
+
+function reduce<T, R>(predicate: (accumulator: T|R, data: T) => R, collection: IterableIterator<T> | Array<T>, initialValue?: R) {
+  let iterator = collection;
   let result = initialValue;
 
   if (initialValue == undefined || initialValue == null) {
-    let iterator = collection[Symbol.iterator]();
+    iterator = collection[Symbol.iterator]();
     result = iterator.next().value;
+  }
 
-    for (const d of iterator) {
-      result = predicate(result as R, d);
-    }
+  for (const d of iterator) {
+    result = predicate(result!, d);
   }
 
   return result;
