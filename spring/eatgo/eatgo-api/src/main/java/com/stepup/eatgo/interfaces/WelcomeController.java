2ae2215611dd1f35104a406e790dd6d346930786
jinseok <metocherry@gmail.com>
2020-07-01 00:15:27 +0900

    Add spring boot example

---
+++ spring/eatgo/eatgo-api/src/main/java/com/stepup/eatgo/interfaces/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.stepup.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// RestController - 웹에서 접속 가능하게 만들어준다.
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!";
+    }
+}
