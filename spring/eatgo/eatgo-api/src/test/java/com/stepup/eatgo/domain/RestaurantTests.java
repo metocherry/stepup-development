5149c9bb1089b343e8083762e08538b20c7a80f2
jinseok <metocherry@gmail.com>
2020-07-02 07:01:47 +0900

    Add sample test code

---
+++ spring/eatgo/eatgo-api/src/test/java/com/stepup/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,21 @@
+package com.stepup.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTests {
+
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertEquals("Bob zip", restaurant.getName());
+        assertEquals("Seoul", restaurant.getAddress());
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertEquals("Bob zip in Seoul", restaurant.getInformation());
+    }
+}
