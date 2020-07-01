dd1b138474b180c04a48614f14827d7fa203ea12
jinseok <metocherry@gmail.com>
2020-07-02 07:37:00 +0900

    Add demo rest get api

--- spring/eatgo/eatgo-api/src/test/java/com/stepup/eatgo/domain/RestaurantTest.java
+++ spring/eatgo/eatgo-api/src/test/java/com/stepup/eatgo/domain/RestaurantTest.java
@@ -4,18 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RestaurantTests {
+class RestaurantTest {
 
     @Test
     public void creation() {
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+
+        assertEquals(1004L, restaurant.getId());
         assertEquals("Bob zip", restaurant.getName());
         assertEquals("Seoul", restaurant.getAddress());
     }
 
     @Test
     public void information() {
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
         assertEquals("Bob zip in Seoul", restaurant.getInformation());
     }
 }
---
+++ spring/eatgo/eatgo-api/src/test/java/com/stepup/eatgo/interfaces/RestaurantControllerTest.java
@@ -0,0 +1,39 @@
+package com.stepup.eatgo.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// @ExtendWith(SpringExtension.class)
+// -> 요청에 대해서 스프링을 이용하여 테스트할 수 있게 해준다.
+// @WebMvcTest
+// -> 특정 컨트롤러를 테스트한다는 것을 명시한다.
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(RestaurantController.class)
+class RestaurantControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void list() throws Exception {
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")
+                ))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Bob zip\"")
+                ));
+    }
+}
