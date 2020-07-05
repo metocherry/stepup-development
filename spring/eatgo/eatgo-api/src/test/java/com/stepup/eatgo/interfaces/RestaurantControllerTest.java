a1c48507a4975af20ad97fb5fde40225e31b1335
jinseok <metocherry@gmail.com>
2020-07-05 23:16:52 +0900

    Update install of nodejs

--- spring/eatgo/eatgo-api/src/test/java/com/stepup/eatgo/interfaces/RestaurantControllerTest.java
+++ spring/eatgo/eatgo-api/src/test/java/com/stepup/eatgo/interfaces/RestaurantControllerTest.java
@@ -28,12 +28,33 @@ class RestaurantControllerTest {
     @Test
     public void list() throws Exception {
         mvc.perform(get("/restaurants"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(
-                        containsString("\"id\":1004")
-                ))
-                .andExpect(content().string(
-                        containsString("\"name\":\"Bob zip\"")
-                ));
+            .andExpect(status().isOk())
+            .andExpect(content().string(
+                containsString("\"id\":1004")
+            ))
+            .andExpect(content().string(
+                containsString("\"name\":\"Bob zip\"")
+            ));
+    }
+
+    @Test
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/1004"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(
+                containsString("\"id\":1004")
+            ))
+            .andExpect(content().string(
+                containsString("\"name\":\"Bob zip\"")
+            ));
+
+        mvc.perform(get("/restaurants/2020"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(
+                containsString("\"id\":2020")
+            ))
+            .andExpect(content().string(
+                containsString("\"name\":\"Cyber Food\"")
+            ));
     }
 }
--- spring/eatgo/scripts/bootstrap.sh
+++ spring/eatgo/scripts/bootstrap.sh
@@ -16,3 +16,13 @@ source ~/.bashrc
 
 java -version
 javac -version
+
+
+## NodeJS
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install --lts
+node -v
+npm -v
