d8063b02221e109fba47c65395952c5959be7eab
jinseok <metocherry@gmail.com>
2020-07-18 12:52:24 +0900

    Implement linked list

--- data-structure/packages/linked-list/src/linked-list.test.ts
+++ data-structure/packages/linked-list/src/linked-list.test.ts
@@ -57,5 +57,129 @@ describe("[LinkedList]", () => {
             expect(list.find(2)).toBe(30);
             expect(list.size()).toBe(4);
         });
-    })
+    });
+
+    describe("사용자는 하나의 데이터를 삭제할 수 있다.", () => {
+        it("리스트가 비어 있다면, 에러가 발생한다.", () => {
+            const list = new LinkedList<number>();
+            expect(() => list.remove()).toThrow();
+        });
+
+        it("리스트에 데이터가 있다면, 마지막 데이터를 삭제한다.", () => {
+            const list = new LinkedList<number>();
+            list.add(10);
+            list.add(20);
+            list.add(30);
+
+            list.remove();
+
+            expect(list.size()).toBe(2);
+            expect(list.findLast()).toBe(20);
+        });
+    });
+
+    describe("사용자는 특정 인덱스의 데이터를 삭제할 수 있다.", () => {
+        it("리스트가 비어 있다면, 에러가 발생한다.", () => {
+            const list = new LinkedList<number>();
+            expect(() => list.removeAt(0)).toThrow();
+        });
+
+        it("인덱스가 유효하지 않은면, 에러가 발생한다.", () => {
+            const list = new LinkedList<number>();
+            list.add(10);
+
+            expect(() => list.removeAt(1)).toThrow();
+        });
+
+        it ("리스트에 데이터가 한 개 존재할 때, 인덱스 0 의 데이터를 삭제한다.", () => {
+            const list = new LinkedList<number>();
+            list.add(10);
+
+            list.removeAt(0);
+
+            expect(list.size()).toBe(0);
+            expect(() => list.findFirst()).toThrow();
+        })
+
+        it("리스트에 데이터가 세 개 존재할 때, 인덱스 1 의 데이터를 삭제한다.", () => {
+            const list = new LinkedList<number>();
+            list.add(10);
+            list.add(20);
+            list.add(30);
+
+            list.removeAt(1);
+
+            expect(list.size()).toBe(2);
+            expect(list.find(1)).toBe(30);
+        });
+    });
+
+    it("사용자는 리스트를 비울 수 있다.", () => {
+        const list = new LinkedList<number>();
+        list.add(10);
+        list.add(20);
+
+        list.clear();
+
+        expect(list.size()).toBe(0);
+    });
+
+    describe("사용자는 배열이 비어있는지 확인할 수 있다.", () => {
+        it("리스트가 비어있다면, true를 리턴한다.", () => {
+            const list = new LinkedList<number>();
+
+            expect(list.isEmpty()).toBe(true);
+        });
+
+        it("리스트에 데이터가 있다면, false를 리턴한다.", () => {
+            const list = new LinkedList<number>();
+
+            list.add(10);
+
+            expect(list.isEmpty()).toBe(false);
+        });
+    });
+
+    describe("사용자는 입력된 데이터와 가장 먼저 일치하는 데이터의 인덱스를 찾을 수 있다.", () => {
+        it("일치하는 데이터가 없다면, -1을 리턴한다.", () => {
+            const list = new LinkedList<number>();
+
+            expect(list.indexOf(0)).toBe(-1);
+        });
+
+        it("일치하는 데이터가 있다면, 데이터가 위치한 인덱스를 리턴한다.", () => {
+            const list = new LinkedList<number>();
+            list.add(10);
+            list.add(20);
+            list.add(30);
+
+            expect(list.indexOf(30)).toBe(2);
+        });
+    });
+
+    describe("사용자는 배열에 입력된 데이터가 존재하는지 판단할 수 있다.", () => {
+        it("일차하는 데이터가 없다면, false를 리턴한다.", () => {
+            const list = new LinkedList<number>();
+
+            expect(list.contain(10)).toBe(false);
+        });
+
+        it("일차하는 데이터가 있다면, true를 리턴한다.", () => {
+            const list = new LinkedList<number>();
+            list.add(10);
+            list.add(20);
+            list.add(30);
+
+            expect(list.contain(10)).toBe(true);
+        });
+    });
+
+    it("사용자는 배열을 순회할 수 있다.", () => {
+        const list = new LinkedList<number>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        expect([...list]).toEqual([10, 20, 30]);
+    });
 });
--- data-structure/packages/linked-list/src/linked-list.ts
+++ data-structure/packages/linked-list/src/linked-list.ts
@@ -1,19 +1,100 @@
 import { LinkedNode } from "./linked-node";
-import { cursorTo } from "readline";
 
-export class LinkedList<T> {
+export class LinkedList<T> implements Iterable<T> {
     #head?: LinkedNode<T>;
+
     #tail?: LinkedNode<T>;
 
     #pointer: number = -1;
 
     constructor() {}
 
+    [Symbol.iterator](): IterableIterator<T> {
+        let node: LinkedNode<T> | undefined;
+
+        return {
+            next: (): IteratorResult<T, undefined> => {
+                node = node ? node.next : this.#head;
+
+                return node
+                    ? { done: false, value: node!.value }
+                    : { done: true, value: undefined };
+            },
+            [Symbol.iterator](): IterableIterator<T> {
+                return this;
+            }
+        };
+    }
+
     /**
      * 현재 크기를 리턴한다.
      */
     public size(): number {
         return this.#pointer + 1;
+    }
+
+    /**
+     * 인덱스에 위치한 데이터를 조회한다.
+     * @param index
+     */
+    public find(index: number): T | undefined {
+        if (!this.isValidIndex(index)) {
+            throw this.invalidIndexError();
+        }
+
+        return this.findNode(index)?.value;
+    }
+
+    /**
+     * 첫번쩨 데이터를 조회한다.
+     */
+    public findFirst(): T | undefined {
+        this.assertEmpty();
+        return this.#head?.value;
+    }
+
+    /**
+     * 마지막 데이터를 조회한다.
+     */
+    public findLast(): T | undefined {
+        this.assertEmpty();
+        return this.#tail?.value;
+    }
+
+    /**
+     * 데이터와 일치하는 인덱스를 조회한다.
+     * @param data
+     */
+    public indexOf(data: T): number {
+        let node = this.#head;
+
+        for (let i = 0; i < this.#pointer + 1; i++) {
+            if (node!.value == data) {
+                return i;
+            }
+
+            node = node!.next;
+        }
+
+        return -1;
+    }
+
+    /**
+     * 입력된 데이터를 가지고 있는지 판단한다.
+     * @param data
+     */
+    public contain(data: T): boolean {
+        let node = this.#head;
+
+        for (let i = 0; i < this.#pointer + 1; i++) {
+            if (node!.value == data) {
+                return true;
+            }
+
+            node = node!.next;
+        }
+
+        return false;
     }
 
     /**
@@ -35,26 +116,12 @@ export class LinkedList<T> {
     }
 
     /**
-     * 인덱스에 위치한 데이터를 조회한다.
-     * @param index
-     */
-    public find(index: number): T | undefined {
-        if (!this.isValidIndex(index)) {
-            throw this.invalidIndexError();
-        }
-
-        return this.findNode(index)?.value;
-    }
-
-    /**
      * 특정 인덱스에 데이터를 추가한다.
      * @param index
      * @param data
      */
     public addAt(index: number, data: T): void {
-        if (!this.isValidIndex(index)) {
-            throw this.invalidIndexError();
-        }
+        this.asserValidIndex(index);
 
         const newNode = new LinkedNode(data);
 
@@ -69,6 +136,55 @@ export class LinkedList<T> {
         }
 
         this.#pointer++;
+    }
+
+    /**
+     * 마지막 노드부터 순선대로 데이터를 삭제한다.
+     */
+    public remove(): void {
+        this.assertEmpty();
+
+        const node = this.findNode(this.#pointer - 1);
+        node!.next = undefined;
+        this.#tail = node;
+
+        this.#pointer--;
+    }
+
+    /**
+     * 인덱스와 일치하는 데이터를 삭제한다.
+     * @param index
+     */
+    public removeAt(index: number): void {
+        this.asserValidIndex(index);
+
+        if (index == 0) {
+            const node = this.#head!.next;
+            this.#head = node;
+        }
+        else {
+            const node = this.findNode(index - 1);
+            const temp = node!.next!.next;
+            node!.next = temp;
+        }
+
+        this.#pointer--;
+    }
+
+    /**
+     * 리스트가 비어있는데 판단한다.
+     */
+    public isEmpty(): boolean {
+        return this.#pointer < 0;
+    }
+
+    /**
+     * 리스트를 비운다.
+     */
+    public clear(): void {
+        this.#head = undefined;
+        this.#tail = undefined;
+        this.#pointer = -1;
     }
 
     /**
@@ -93,10 +209,25 @@ export class LinkedList<T> {
         return index >= 0 && index <= this.#pointer;
     }
 
-    /**
-     * 유효하지 않은 인덱스 에러를 리턴한다.
-     */
     private invalidIndexError(): Error {
         return new Error("The index is invalid");
+    }
+
+    /**
+     * 인덱스가 유효하지 않으면 에러를 발생시킨다.
+     */
+    private asserValidIndex(index: number): never | void {
+        if (!this.isValidIndex(index)) {
+            throw new Error("The index is invalid");
+        }
+    }
+
+    /**
+     * 리스트가 비어있다면 에러를 발생시킨다.
+     */
+    private assertEmpty(): never | void {
+        if (this.isEmpty()) {
+            throw new Error("The LinkedList is empty.");
+        }
     }
 }
